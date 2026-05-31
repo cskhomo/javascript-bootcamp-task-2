81fe1dd4dda8d3324147bbfe0e931f3e91e5e36e
Chrisser <164577349+cskhomo@users.noreply.github.com>
2026-06-01 00:50:33 +0200

    Add submission handler

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const table = document.querySelector("table");
 const rows = document.querySelector("tbody");
 
 let students = [];
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    
+    let name = document.getElementById("name").value.trim();
+    let mark = parseInt(document.getElementById("mark").value);
+    
+    let result = mark >= 50 ? "PASS" : "FAIL"
+    let grade = "";
+}
