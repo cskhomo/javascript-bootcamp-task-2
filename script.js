3e725f66f8c6d3f3b3678e53316518b3090e412e
Chrisser <164577349+cskhomo@users.noreply.github.com>
2026-06-01 01:04:22 +0200

    Refactor submission and grading

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const form = document.querySelector("form");
-const table = document.querySelector("table");
 const rows = document.querySelector("tbody");
-
 let students = [];
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault(); 
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
     
     let name = document.getElementById("name").value.trim();
     let mark = parseInt(document.getElementById("mark").value);
+    let result = mark >= 50 ? "PASS" : "FAIL";
     
-    let result = mark >= 50 ? "PASS" : "FAIL"
-    let grade = "";
-    
-    if (mark >= 80) grade = "Distinction";
-    else if (mark >= 65) grade = "Merit";
-    else if (mark >= 50) grade = "Pass";
-    else grade = "Fail";
+    let grade = get_grade(mark);
+    const student = create_student(name, mark, result, grade);
+    add_row(student);
 
+    form.reset();
+});
+
+function get_grade(mark) {
+    if (mark >= 80) return "Distinction";
+    if (mark >= 65) return "Merit";
+    if (mark >= 50) return "Pass";
+    return "Fail";
+}
+
+function create_student(name, mark, result, grade) {
     let student = {name, mark, result, grade};
-    students.push(student);
-    
+    students.push(student)
+    return student
+}
+
+function add_row(student) {
     rows.innerHTML += `
         <tr>
             <td>${student.name}</td>
@@ -29,6 +38,4 @@ form.addEventListener("submit", function(e) {
             <td>${student.grade}</td>
         </tr>
     `;
-
-    form.reset();
-});
+}
